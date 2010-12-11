f9d21ff6db53de23fa8998adf6459afc21d383b8
Alexey Romanov <alexey.v.romanov@gmail.com>
2010-12-11 22:23:10 +0300

    Batch file to build the port driver on Windows

---
+++ build_win.bat
@@ -0,0 +1,8 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+set ERL_ROOT="F:\ProgLangs\erl"
+set ERL_INTERFACE="%ERL_ROOT%\lib\erl_interface-3.7.2"
+set ERTS="%ERL_ROOT%\erts-5.8.2"
+set SQLITE_SRC="F:\MyProgramming\sqlite-amalgamation"
+cl -I%ERL_INTERFACE%/include -I%ERTS%/include -I%SQLITE_SRC% c_src/*.c /link /dll %ERL_INTERFACE%\lib\ei.lib %SQLITE_SRC%\sqlite3.lib /out:priv\sqlite3_drv.dll
+
